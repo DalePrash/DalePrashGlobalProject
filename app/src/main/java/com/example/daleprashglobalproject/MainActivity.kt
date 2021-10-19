e01ebce75f7e48350422039ee0be2461d2772eb2
Dalesh Prashar <prashdale@gmail.com>
2021-10-18 21:41:41 -0500

    second

--- app/src/main/java/com/example/daleprashglobalproject/MainActivity.kt
+++ app/src/main/java/com/example/daleprashglobalproject/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // lets see if it works
-        // fkjkjkjlkjdskj second
+        
     }
 }
